332cf78010f26c6aaf414ed2243c7399f3b11600
Praneeth Mekapati <pm14@illinois.edu>
2020-11-30 01:46:13 -0600

    commiting

--- algorithms.cpp
+++ algorithms.cpp
@@ -8,15 +8,88 @@
 
 using namespace std;
 
+bool extractDouble(string txt){
+    istringstream iss(txt);
+    double d;
+    return iss >> d >> ws && iss.eof();
+}
+
+bool extractInt(string txt){
+    istringstream iss(txt);
+    int d;
+    return iss >> d >> ws && iss.eof();
+}
+
+long double toRadians(const long double degree) 
+{ 
+    // cmath library in C++  
+    // defines the constant 
+    // M_PI as the value of 
+    // pi accurate to 1e-30 
+    double pi = 2 * acos(0);
+    long double one_deg = (pi) / 180; 
+    return (one_deg * degree); 
+} 
+  
+long double distance(long double lat1, long double long1,  
+                     long double lat2, long double long2) 
+{ 
+    // Convert the latitudes  
+    // and longitudes 
+    // from degree to radians. 
+    lat1 = toRadians(lat1); 
+    long1 = toRadians(long1); 
+    lat2 = toRadians(lat2); 
+    long2 = toRadians(long2); 
+      
+    // Haversine Formula 
+    long double dlong = long2 - long1; 
+    long double dlat = lat2 - lat1; 
+  
+    long double ans = pow(sin(dlat / 2), 2) +  
+                          cos(lat1) * cos(lat2) *  
+                          pow(sin(dlong / 2), 2); 
+  
+    ans = 2 * atan2(sqrt(ans), sqrt(1 - ans)); 
+  
+    // Radius of Earth in  
+    // Kilometers, R = 6371 
+    // Use R = 3956 for miles 
+    long double R = 6371; 
+      
+    // Calculate the result 
+    ans = ans * R; 
+  
+    return ans; 
+} 
+
 void Algorithms::insert(const string & filename){
-    string line;
-    ifstream myfile (filename);
-    if (myfile.is_open() == true){
-        while (getline(myfile, line) == true){
-            Flights_Available.push_back(line);
+    string txt;
+    ifstream myfile(filename);
+    int c = 1;
+    string tmp;
+    while (getline (myfile, txt, ',')){
+        if (extractDouble(txt) == true && extractInt(txt) == false && c <= 2){
+            if (c == 1){
+                tmp = txt;
+            } else{
+                coordinates.push_back(make_pair(stod(tmp), stod(txt)));
+            }
+            c++;
+        }
+        if (c == 2){
+            c = 0;
         }
     }
-    size_ = Flights_Available.size();
+    myfile.close();
+}
+
+void calculateDistances(vector< pair<double, double> > coordinates_){
+     double x = (double) rand() / RAND_MAX;
+    double y = (double) rand() / RAND_MAX;
+    for (int idx = 0; idx < coordinates_.size(); idx++){
+        distances.push_back(distance(x, y, coordinates_[idx].first, coordinates_[idx].second));
+    }
 }
 
 int **Algorithms::createGrid(const string & filename){
--- algorithms.h
+++ algorithms.h
@@ -1,25 +1,31 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <cstdlib>
-#include <string>
 #include <vector>
-
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <stdlib.h>
+#include <typeinfo>
+#include <utility>
+#include <math.h>
 using namespace std;
 
 class Algorithms {
 
     public:
         void insert(const string & filename); // gets the string of available flights into the vector
+        void calculateDistances(vector< pair<double, double> > coordinates_);
+        long double toRadians(const long double degree);
+        long double distance(long double lat1, long double long1, long double lat2, long double long2);
         int **createGrid(const string & filename); // creates a graph of locations
         int **Dijkstra(); // sorts the graph based on the algorithm of Dijkstra
+        bool extractDouble(string txt);
+        bool extractInt(string txt);
 
 
     private:
-        vector<string> Flights_Available;
-        vector<double> coordinates_; // 
+        vector< pair<double, double> > coordinates;
+        vector<double> distances;
         int size_; // size of array storing the flights available and the coordinates array
-        double longitude; // current location longitude
-        double lattitude; // current location lattitude
 };
