24b3233c49249105c3d884a98ec35998c72e5860
Praneeth Mekapati <pm14@illinois.edu>
2020-12-01 01:57:16 -0600

    co

--- algorithms.cpp
+++ algorithms.cpp
@@ -16,10 +16,6 @@ bool Algorithms::extractInt(string txt){
 
 long double Algorithms::toRadians(const long double degree) 
 { 
-    // cmath library in C++  
-    // defines the constant 
-    // M_PI as the value of 
-    // pi accurate to 1e-30 
     double pi = 2 * acos(0);
     long double one_deg = (pi) / 180; 
     return (one_deg * degree); 
@@ -28,30 +24,21 @@ long double Algorithms::toRadians(const long double degree)
 long double Algorithms::distance(long double lat1, long double long1,  
                      long double lat2, long double long2) 
 { 
-    // Convert the latitudes  
-    // and longitudes 
-    // from degree to radians. 
+
     lat1 = toRadians(lat1); 
     long1 = toRadians(long1); 
     lat2 = toRadians(lat2); 
     long2 = toRadians(long2); 
       
-    // Haversine Formula 
-    long double dlong = long2 - long1; 
-    long double dlat = lat2 - lat1; 
   
-    long double ans = pow(sin(dlat / 2), 2) +  
+    long double ans = pow(sin((lat2 - lat1) / 2), 2) +  
                           cos(lat1) * cos(lat2) *  
-                          pow(sin(dlong / 2), 2); 
+                          pow(sin((long2 - long1) / 2), 2); 
   
     ans = 2 * atan2(sqrt(ans), sqrt(1 - ans)); 
   
-    // Radius of Earth in  
-    // Kilometers, R = 6371 
-    // Use R = 3956 for miles 
     long double R = 6371; 
       
-    // Calculate the result 
     ans = ans * R; 
   
     return ans; 
