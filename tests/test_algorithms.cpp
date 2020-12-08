db9b3ebc4f453c09663bc5a08a00efa735cfdc8f
Alaa Shuaibi <ashuai6@illinois.edu>
2020-12-07 22:57:50 -0600

    prepared test cases for algorithms

--- tests/test_algorithms.cpp
+++ tests/test_algorithms.cpp
@@ -1,2 +1,14 @@
 #include "catch/catch.hpp"
 #include "../algorithms.h"
+
+TEST_CASE("BFS traverses through the entire component containing start.") {}
+
+TEST_CASE("BFS traverses through the entire graph.") {}
+
+TEST_CASE("DFS traverses through the entire component containing start.") {}
+
+TEST_CASE("DFS traverses through the entire graph.") {}
+
+TEST_CASE("Dijkstra returns correct path for a simple example.") {}
+
+TEST_CASE("Landmark returns correct path for a simple example.") {}
