05f0fc5c304c83fb2fc19da446f081d1647a2c95
Alaa Shuaibi <ashuai6@illinois.edu>
2020-12-08 09:32:22 -0600

    commit before pull

--- algorithms.h
+++ algorithms.h
@@ -19,7 +19,7 @@ class Algorithms {
          * @param visited Used by BFS(Graph g) to keep track of visited airports. 
          * @param path Used by BFS(Graph g) generate a path of airports in the order they were visited.
          * @return A vector of all visited airports in the order they were visited. 
-         * If path is passed in with at least 1 element, it will be adjusted to contain the newly visited airports and then returned.
+         * If path is passed in with at least 1 element, it will be adjusted to contain the newly visited airports and then be returned.
          */
         std::vector<AirportID> BFS(Graph g, AirportID start, std::unordered_map<AirportID, bool> & visited, std::vector<AirportID> path);
 
@@ -36,7 +36,7 @@ class Algorithms {
          * @param visited Used by BFS(Graph g) to keep track of visited airports. 
          * @param path Used by BFS(Graph g) generate a path of airports in the order they were visited.
          * @return A vector of all visited airports in the order they were visited. 
-         * If path is passed in with at least 1 element, it will be adjusted to contain the newly visited airports and then returned.
+         * If path is passed in with at least 1 element, it will be adjusted to contain the newly visited airports and then be returned.
          */
         std::vector<AirportID> DFS(Graph g, AirportID start, std::unordered_map<AirportID, bool> & visited, std::vector<AirportID> path);
 
