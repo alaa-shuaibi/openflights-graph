a545319d19f44fed72645eeba0c24392fb74bfd5
Rahul Vasanth <rvasant2@siebl-0224-34.ews.illinois.edu>
2020-12-07 18:35:39 -0600

    implemented pseudocode, UNTESTED

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,5 +1,6 @@
 #include "algorithms.h"
 #include <queue>
+#include <map>
 
 /**
  * @param g - the graphs based on OpenFlights dataset
@@ -10,21 +11,37 @@
  * */
 void Algorithms::BFS(Graph g, AirportID start) {
     //no need to distinguish between CROSS and DISCOVERY edges
-    queue<AirportID> q;
-    //setLabel (V, VISITED)
-    //q.push(v)
+    std::map<AirportID, bool> m;
+    m[start] = true;
 
-    //while !q.empty();
-        //v = q.dequeue()
+    std::queue<AirportID> q;
+
+    //setLabel (V, VISITED)
+    q.push(start);
+    
+
+    AirportID temp;
+    
+    while(!q.empty()){
+        temp = q.front();
         //foreach (Vertex w: G.adjacent*v)
+        for(AirportID it: g.getAdjacentAirports(temp)){
             //if getLabel(w) == UNEXPLORED
+            if(m.find(it) == m.end()){//if true m[it] has not been assigned
                 //setLabel(w, VISITED)
+                m[it] = true;
                 //setLabel(v, w, DISCOVERY)
+
                 //q.enqueue(w);
+                q.push(it);
+            }
+            
             //elseif getLabel(v, w) == UNEXPLORED
                 //setLabel(v, w, CROSS)
+        }
+        q.pop();
 
-
+    }
     //BFS(Graph g)
 }
 
