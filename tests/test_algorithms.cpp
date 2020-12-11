3b9e4071c41f5d85a975ac5e2cd0dae059c55b15
Praneeth Mekapati <pm14@illinois.edu>
2020-12-10 22:47:40 -0600

    Added test cases for algo

--- tests/test_algorithms.cpp
+++ tests/test_algorithms.cpp
@@ -45,6 +45,70 @@ TEST_CASE("Dijkstra returns the correct path for a simple example.") {
     REQUIRE(path == "1->2->4->6->9->10");
 }
 
+TEST_CASE("Dijkstra returns the correct path for a more complex example.") {
+    Algorithms algo = Algorithms();
+    
+    std::vector<AirportID> airports;
+
+    for (int i = 1; i <= 10; i++) {
+        airports.push_back(i);
+    }
+
+    Graph g = Graph(airports);
+
+    g.insertEdge(1, 2, 20);
+    g.insertEdge(1, 4, 80);
+    g.insertEdge(1, 7, 90);
+    g.insertEdge(2, 6, 10);
+    g.insertEdge(3, 6, 50);
+    g.insertEdge(3, 8, 20);
+    g.insertEdge(4, 7, 20);
+    g.insertEdge(5, 2, 50);
+    g.insertEdge(5, 7, 30);
+    g.insertEdge(6, 3, 10);
+    g.insertEdge(6, 4, 40);
+    g.insertEdge(7, 1, 20);
+
+    std::string path = algo.Dijkstra(g, 1, 3);
+
+    std::string path2 = algo.Dijkstra(g, 8, 4);
+
+     REQUIRE(path == "1->2->6->3");
+
+     REQUIRE(path2 == "Path doesn't exist.");
+
+}
+
+TEST_CASE("Dijkstra returns the correct path for another complex example.") {
+    Algorithms algo = Algorithms();
+    
+    std::vector<AirportID> airports;
+
+    for (int i = 1; i <= 10; i++) {
+        airports.push_back(i);
+    }
+
+    Graph g = Graph(airports);
+    g.insertEdge(4, 5, 0.35);
+    g.insertEdge(5, 4, 0.35);
+    g.insertEdge(4, 7, 0.37);
+    g.insertEdge(5, 7, 0.28);
+    g.insertEdge(7, 5, 0.28);
+    g.insertEdge(5, 1, 0.32);
+    g.insertEdge(0, 4, 0.38);
+    g.insertEdge(0, 2, 0.26);
+    g.insertEdge(7, 3, 0.39);
+    g.insertEdge(1, 3, 0.29);
+    g.insertEdge(2, 7, 0.34);
+    g.insertEdge(6, 2, 0.40);
+    g.insertEdge(3, 6, 0.52);
+    g.insertEdge(6, 0, 0.58);
+    g.insertEdge(6, 4, 0.93);
+
+    std::string path = algo.Dijkstra(g, 0, 6);
+    std::cout << path << std::endl;
+}
+
 TEST_CASE("Landmark returns the correct path for a simple example.") {
     Algorithms algo = Algorithms();
     
