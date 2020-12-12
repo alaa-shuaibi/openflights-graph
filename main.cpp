292a8bbd58ef7413691517e8d7b9fdf12823c44f
Rahul Vasanth <rvasant2@siebl-0224-07.ews.illinois.edu>
2020-12-11 23:42:23 -0600

    finalized cleaning up main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
 #include "OpenFlights.h"
 
 int main() {
-    /**
-    * type module load llvm/6.0.1
-    * type make 
-    * type ./finalproject
-    * 
-    * modify main as you see fit. 
-    */
-
+//these serve as userInputs. start, end, and checkPoint are used in traversals while input provides user options.
     int input, start, end, checkPoint;
     string fileAirports, fileRoutes;
     //if invalid input is provided twice, end program
@@ -22,13 +15,13 @@ int main() {
     std::cin >> fileAirports;
     std::cout << "Input the name of your (properly formatted) Routes file:";
     std::cin >> fileRoutes;
-
+    //declare Openflights object with the files provided as input
+    //if empty or invalid files the program crashes
     OpenFlights o = OpenFlights(fileAirports, fileRoutes);
     //take the graph that has already been constructed for use of Algorithms
     Graph g = o.getGraph();
     //to call algorithms and traversals
     Algorithms algo = Algorithms();
-
 
     do{
         std::cout << "g.getAllAirports().size() = " << g.getAllAirports().size() << std::endl;
@@ -38,23 +31,18 @@ int main() {
         if(input == 0){ //call DFS
             //std::cout << "DFS call" << std::endl;
             auto airportVec = algo.DFS(g);
-            //iterate through and print
-            
         }
         if(input == 1){ //call BFS
            //std::cout << "BFS call" << std::endl;
             auto airportVec = algo.BFS(g);
-            //iterate through and print
-            
+            //iterate through and print     
         }
         else if(input == 2){ // call Dijkstra's
             std::cout << "Input starting AirportID:";
             std::cin >> start;
             std::cout << "Input ending AirportID:";
             std::cin >> end;
-            auto a = g.getAllAirports();
- 
-
+             
             std::cout << "Dijkstra Path: " << o.pathToString(algo.Dijkstra(g, start, end)) << std::endl;
         }
         else if(input == 3){ //call Landmark path
@@ -67,16 +55,16 @@ int main() {
 
             std::cout << "Landmark Path: " << o.pathToString(algo.Landmark(g, start, end, checkPoint)) << std::endl;
         }
-        else if(input == 4){
+        else if(input == 4){//new files to be inputted
             std::cout << "Input new Airports file:";
             std::cin >> fileAirports;
             std::cout << "Input new Routes file:";
             std::cin >> fileRoutes;
-
+            //make new graph based on new files
             o = OpenFlights(fileAirports, fileRoutes);
             g = o.getGraph();
         }
-        else{
+        else{//one chance for invalid input
             std::cout << "Invalid Input" << std::endl;
             toExit++;
         }
